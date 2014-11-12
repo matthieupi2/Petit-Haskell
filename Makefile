8fd5986835f772c61192f4f1bd3b65316db59a0c
Matthieu Piquerez <matthieu@rezo-159-70.ens.fr>
2014-11-12 22:22:37 +0100

    Définition des éléments de l'AST.

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
 GENERATED = lexer.ml parser.ml parser.mli parser.output parser.automaton petitghc
 
 clean:
-	rm -f .{[!g,.]*,g[!i]*,gi[!t]*} *.cm[iox] *.o *.annot *~ *.native $(GENERATED)
+	rm -f .{[!g]*,g[!i]*,gi[!t]*} *.cm[iox] *.o *.annot *~ *.native $(GENERATED)
--- ast.mli
+++ ast.mli
@@ -1,8 +1,37 @@
 
 (* Arbre de syntaxe abstraite de Petit Haskell *)
 
+type ident0 = Ident0 of string
+
+type ident = string
+
+type unop =
+  | Uneg
+
+type binop =
+  | Badd | Bsub | Bmul
+  | Blt | Bleq | Bgt | Bgeq | Beq | Bneq
+  | Band | Bor | Bcol
+
 type constant =
   | Cint of int
   | Cchar of char
   | Cstr of string
   | Cbool of bool
+
+type def0 =
+  ident0 * ident list * expr
+
+and def =
+  ident * ident list * expr
+
+and expr =
+  | Ecst of constant
+  | Elist of expr list
+  | Ecurr of expr list
+  | Elambda of ident list * expr
+  | Eif of expr * expr * expr
+  | Elet of def list * expr
+  | Ecase of expr * expr * ident * ident * expr
+  | Edo of expr list
+  | Ereturn
