35dfadcf1e64628084c460ed8a4f2ca40dfe2ff2
Matthieu Piquerez <matthieu@MacBook-de-Matthieu-Piquerez.local>
2014-12-15 18:56:43 +0100

    Ajout de IdentError dans Error.

--- error.ml
+++ error.ml
@@ -1,7 +1,10 @@
 type location = Lexing.position * Lexing.position
+type identError = RedefPrimitive | RedefGlobal of location |
+    RedefArg of location | Unbound
 
 exception LexerError of string
 exception ParserError of string
+exception IdentError of string * location * identError
 exception CompilerError of string
 
 (* Pour ne pas avoir à localiser les expressions créées par le compilateur *)
