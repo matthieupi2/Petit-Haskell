12973e16f4db343f933d79bcbc9c381f5b63f146
Matthieu Piquerez <matthieu@MacBook-de-Matthieu-Piquerez.local>
2014-12-25 11:47:00 +0100

    Allégement de print_typed_ast dans Main.

--- main.ml
+++ main.ml
@@ -163,29 +163,29 @@ let print_typed_ast =
     | Cbool true -> printf "True"
     | Cbool false -> printf "False" in
   let rec print_expr e = ( match e.texpr with 
-      | Tident s -> printf " (%s" s
-      | Tcst c -> printf " (" ; print_cte c
-      | Tlist l -> printf " ([" ; List.iter print_expr l ; printf " ]"
+      | Tident s -> printf " %s" s
+      | Tcst c -> printf " " ; print_cte c
+      | Tlist l -> printf " [" ; List.iter print_expr l ; printf " ]"
       | Tappli (f, arg) -> printf " (" ; print_expr f ; printf "(" ;
-        print_expr arg ; printf " )"
-      | Tlambda (args, e) -> printf " ((\\" ;
+        print_expr arg ; printf " ))"
+      | Tlambda (args, e) -> printf " (\\" ;
       List.iter (fun s -> printf " %s" s) args ; printf " ->" ; print_expr e ;
         printf ")"
       | Tbinop (o, e0, e1) -> printf " (%s" (Hashtbl.find ops o) ;
-        print_expr e0 ; print_expr e1
+      print_expr e0 ; print_expr e1 ; printf ")"
       | Tif (cdt, e1, e2) -> printf " (if " ; print_expr cdt ; printf " then " ;
-        print_expr e1 ; printf " else " ; print_expr e2
+        print_expr e1 ; printf " else " ; print_expr e2 ; printf ")"
       | Tlet (ld, e) -> printf "(let " ;
         List.iter (fun d -> print_def d ; printf "\n") ld ; printf "in" ;
-        print_expr e 
-      | Tcase (e, e0, hd, tl, e1) -> printf "(case " ; print_expr e ;
+        print_expr e ; printf ")"
+      | Tcase (e, e0, hd, tl, e1) -> printf "case " ; print_expr e ;
         printf " of\n | [] -> " ; print_expr e0 ; printf "\n | %s:%s -> " hd tl ;
         print_expr e1 ; printf "\n"
       | Tdo l ->
-          printf "({" ; List.iter (fun e -> printf "\n" ; print_expr e) l ;
+          printf "{" ; List.iter (fun e -> printf "\n" ; print_expr e) l ;
         printf "\n}"
-      | Treturn -> printf " (()" ) ;
-    printf ")<%s>" (string_of_typ e.typ var_names)
+      | Treturn -> printf " ()" ) ;
+    printf "<%s>" (string_of_typ e.typ var_names)
   and print_def (s, e) = printf "%s=\n" s ; print_expr e in
   let rec print_file = function
     | [] -> ()
