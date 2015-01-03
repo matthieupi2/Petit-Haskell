57683ca4e12aa6d5463c0e9a582555fed493cbba
alban-pierre <alban.pierre@ens.fr>
2015-01-03 16:16:39 +0100

    Construction des fermetures

--- compile.ml
+++ compile.ml
@@ -89,6 +89,87 @@ let var_libre = List.map var_libre_def
 (************************************************************************************)
 (* phase 2 : construction explicite des fermetures *)
 
+let numfun = ref 0
+
+type fdef =
+  | Fdef of ident * ident list * fexpr
+  | Ffun of ident * ident list * ident * fexpr
+  | Fmain of fexpr
+
+and fexpr =
+  | Fident of ident
+  | Fcst of constant
+  | Flist of fexpr list
+  | Fappli of fexpr * fexpr
+  | Fclos of ident * ident list
+  | Fbinop of binop * fexpr * fexpr
+  | Fif of fexpr * fexpr * fexpr
+  | Flet of ident * fexpr * fexpr
+  | Fcase of fexpr * fexpr * ident * ident * fexpr
+  | Fdo of fexpr list
+  | Freturn
+
+
+let rec ferm_expr = function
+  | {vexpr = Vident i; var_libres = l} -> Fident i, []
+
+  | {vexpr = Vcst c; var_libres = l} -> Fcst c, []
+
+  | {vexpr = Vlist l1; var_libres = l} ->
+       let l2, lf1 = List.fold_left (fun (l3,lf) vv -> let f1, f2 = ferm_expr vv in (f1::l3, f2@lf))
+                                    ([], [])
+                                    l1
+                     in
+       Flist l2, lf1
+
+  | {vexpr = Vappli (v1, v2); var_libres = l} -> 
+       let f1d, f1f = ferm_expr v1 in
+       let f2d, f2f = ferm_expr v2 in
+       Fappli (f1d, f2d), f1f@f2f
+
+  | {vexpr = Vlambda(i, vv); var_libres = l} -> 
+    numfun := !numfun + 1;
+    let f1, f2 = ferm_expr vv in
+    let s = ("_fun" ^ (string_of_int (!numfun))) in
+    Fclos (s, l), Ffun (s, l, i, f1)::f2
+
+  | {vexpr = Vbinop (o, v1, v2); var_libres = l} -> 
+       let f1d, f1f = ferm_expr v1 in
+       let f2d, f2f = ferm_expr v2 in
+       Fbinop (o, f1d, f2d), f1f@f2f
+
+  | {vexpr = Vif (v1, v2, v3); var_libres = l} -> 
+       let f1d, f1f = ferm_expr v1 in
+       let f2d, f2f = ferm_expr v2 in
+       let f3d, f3f = ferm_expr v3 in
+       Fif (f1d, f2d, f3d), f1f@f2f@f3f
+
+  | {vexpr = Vlet (i, v1, v2); var_libres = l} -> 
+       let f1d, f1f = ferm_expr v1 in
+       let f2d, f2f = ferm_expr v2 in
+       Flet (i, f1d, f2d), f1f@f2f
+
+  | {vexpr = Vcase (v1, v2, i1, i2, v3); var_libres = l} -> 
+       let f1d, f1f = ferm_expr v1 in
+       let f2d, f2f = ferm_expr v2 in
+       let f3d, f3f = ferm_expr v3 in
+       Fcase (f1d, f2d, i1, i2, f3d), f1f@f2f@f3f
+
+  | {vexpr = Vdo l1; var_libres = l} ->
+       let l2, lf1 = List.fold_left (fun (l3,lf) vv -> let f1, f2 = ferm_expr vv in (f1::l3, f2@lf))
+                                    ([], [])
+                                    l1
+                     in
+       Fdo l2, lf1
+
+  | {vexpr = Vreturn; var_libres = l} -> Freturn, []
+
+
+let ferm_def = function
+  | Vdef ("main" , v) -> let fmain, ffun = ferm_expr v in (Fmain fmain)::ffun
+  | Vdef (i, v) -> let fmain, ffun = ferm_expr v in (Fdef (i, v.var_libres, fmain))::ffun
+
+let ferm p = List.concat (List.map ferm_def p)
 
 
 
