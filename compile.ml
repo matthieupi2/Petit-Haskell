1a10e709938764127c96bc8f1b996ef2d81bdd44
alban-pierre <alban.pierre@ens.fr>
2015-01-04 17:32:48 +0100

    Codage de compile_expr

--- compile.ml
+++ compile.ml
@@ -195,14 +195,14 @@ and cexpr =
   | CBool of bool
   | CList of cexpr list
   | CAppli of cexpr * cexpr
-  | CClos of ident * ident list
+  | CClos of ident * int list
   | CBinop of binop * cexpr * cexpr
   | CIf of cexpr * cexpr * cexpr
   | CLet of int * cexpr * cexpr
   | CCase of cexpr * cexpr * int * int * cexpr
   | CDo of cexpr list
   | CReturn
-  | CGlacon of ident * ident list
+  | CGlacon of ident * int list
 
 
 exception VarUndef of string
@@ -217,7 +217,7 @@ let rec alloc_expr env next = function
   | Fident x -> (
       try
         let adr = Smap.find x env in CLvar adr
-      with
+      with (* ne doit jamais arriver : les variables globales sont des variables libres et sont donc dans l'environnement *)
         | Not_found -> if Hashtbl.mem genv x then CGvar x else raise (VarUndef x)
                 ), next
 
@@ -240,7 +240,8 @@ let rec alloc_expr env next = function
      let a2, n2 = alloc_expr env next e2 in
      CAppli (a1, a2), max n1 n2
 
-  | Fclos (i, l) -> CClos (i, l), next
+  | Fclos (i, l) -> let l1 = List.fold_left (fun l2 x -> (Smap.find x env)::l2) [] l in
+                    CClos (i, l1), next
 
   | Fbinop (o, e1, e2) ->
      let a1, n1 = alloc_expr env next e1 in
@@ -274,7 +275,8 @@ let rec alloc_expr env next = function
 
   | Freturn -> CReturn, next
 
-  | Fglacon (i, l) -> CGlacon (i, l), next
+  | Fglacon (i, l) -> let l1 = List.fold_left (fun l2 x -> (Smap.find x env)::l2) [] l in
+                      CGlacon (i, l1), next
 
 (*
   | PCst i ->
@@ -350,14 +352,115 @@ let alloc_def = function
 let alloc = List.map alloc_def
 
 (******************************************************************************)
-(* phase 2 : production de code *)
+(* phase 4 : production de code *)
+
+let numlbl = ref 0
 
 let pushn n = sub sp sp oi n
 
 let rec compile_expr = function
+  | CLvar adr -> lw a0 areg(adr, fp)
+
+  | CGvar i -> la a0 alab i
+
+  | CInt i -> li v0 9 ++ li a0 8 ++ syscall ++
+              sw zero areg(0, v0) ++ li a0 i ++ sw a0 areg(4, v0) ++ move a0 v0
+
+  | CChar c -> li v0 9 ++ li a0 8 ++ syscall ++
+               sw zero areg(0, v0) ++ la a0 alab ("_char_" ^ (string_of_int (int_of_char c))) ++
+               sw a0 areg(4, v0) ++ move a0 v0
+
+  | CBool b -> li v0 9 ++ li a0 8 ++ syscall ++
+               sw zero areg(0, v0) ++ la a0 alab (if b then "_bool_True" else "_bool_False") ++
+               sw a0 areg(4, v0) ++ move a0 v0
+
+  | CList [] -> li v0 9 ++ li a0 8 ++ syscall ++
+                sw zero areg(0, v0) ++ sw zero areg(4, v0) ++ move a0 v0
+
+  | CList (a::q) -> let code = compile_expr (CList q) in
+                    code ++ push a0 ++ compile_expr a ++ move a1 a0 ++ pop a2 ++
+                    li v0 9 ++ li a0 12 ++ syscall ++ li a0 1 ++
+                    sw a0 areg(0, v0) ++ sw a1 areg(4, v0) ++ sw a2 areg(8, v0) ++ move a0 v0
+
+  | CAppli (e1, e2) -> let code_e1 = compile_expr e1 in
+                       let code_e2 = compile_expr e2 in
+                       code_e1 ++ push a0 ++ code_e2 ++ pop a1 ++ jalr a1
+
+  | CClos (f, l) -> let c, n1 = List.fold_left (fun (code,n) y ->
+                                                code ++ lw a0 areg(y, fp) ++ sw a0 areg(n, v0)
+                                                , n+4 )
+                                               (nop, 8) l in
+                    li a0 (4*(List.length l) + 8) ++ li v0 9 ++ syscall ++
+                    li a0 2 ++ sw a0 areg(0, v0) ++ la a0 alab f ++ sw a0 areg(4, v0) ++ c ++
+                    move a0 v0
+
+  | CBinop (o, e1, e2) -> 
+      numlbl := !numlbl + 1;
+      let s = ("_lbl_" ^ (string_of_int (!numlbl))) in
+      let code_e1 = compile_expr e1 in
+      let code_e2 = compile_expr e2 in
+      code_e1 ++ push a0 ++ code_e2 ++ push ra ++ jal "_force" ++
+      lw a1 areg (4, sp) ++ sw a0 areg (4, sp) ++ move a0 a1 ++ jal "_force" ++
+      pop ra ++ pop a1 ++ lw a0 areg (4, a0) ++ lw a1 areg (4, a1) ++ (
+      match o with
+	| Badd -> add a2 a0 oreg a1
+        | Bsub -> sub a2 a0 oreg a1
+        | Bmul -> mul a2 a0 oreg a1
+        | Band -> move a2 a0 ++ beq a2 a1 s ++ la a2 alab "_bool_False" ++ label s
+        | Bor -> move a2 a0 ++ beq a2 a1 s ++ la a2 alab "_bool_True" ++ label s
+        | Blt -> la a2 alab "_bool_True" ++ blt a0 a1 s ++ la a2 alab "_bool_False" ++ label s
+        | Bleq -> la a2 alab "_bool_True" ++ ble a0 a1 s ++ la a2 alab "_bool_False" ++ label s
+        | Bgt -> la a2 alab "_bool_True" ++ bgt a0 a1 s ++ la a2 alab "_bool_False" ++ label s
+        | Bgeq -> la a2 alab "_bool_True" ++ bge a0 a1 s ++ la a2 alab "_bool_False" ++ label s
+        | Beq -> la a2 alab "_bool_True" ++ beq a0 a1 s ++ la a2 alab "_bool_False" ++ label s
+        | Bneq -> la a2 alab "_bool_True" ++ bne a0 a1 s ++ la a2 alab "_bool_False" ++ label s
+        | Bcol -> nop
+      ) ++
+      li a0 8 ++ li v0 9 ++ syscall ++
+      sw zero areg(0, v0) ++ sw a2 areg(4, v0) ++ move a0 v0
+  
+  | CIf (e1, e2, e3) ->
+      numlbl := !numlbl + 2;
+      let s1 = ("_lbl_" ^ (string_of_int (!numlbl-1))) in
+      let s2 = ("_lbl_" ^ (string_of_int (!numlbl))) in
+      let code_e1 = compile_expr e1 in
+      let code_e2 = compile_expr e2 in
+      let code_e3 = compile_expr e3 in
+      code_e1 ++ push ra ++ jal "_force" ++ pop ra ++ lw a1 areg (4, a0) ++ la a2 alab "_bool_True"
+      ++ beq a1 a2 s1 ++ code_e2 ++ beq zero zero s2 ++ label s1 ++ code_e3 ++ label s2
+
+  | CLet (adr, e1, e2) -> let code_e1 = compile_expr e1 in
+                          let code_e2 = compile_expr e2 in
+                          code_e1 ++ sw a0 areg(adr, fp) ++ code_e2
+
+  | CCase (e1, e2, adr1, adr2, e3) -> 
+      numlbl := !numlbl + 2;
+      let s1 = ("_lbl_" ^ (string_of_int (!numlbl-1))) in
+      let s2 = ("_lbl_" ^ (string_of_int (!numlbl))) in
+      let code_e1 = compile_expr e1 in
+      let code_e2 = compile_expr e2 in
+      let code_e3 = compile_expr e3 in
+      code_e1 ++ push ra ++ jal "_force" ++ pop ra ++ lw a1 areg (0, a0) ++
+      bne a1 zero s1 ++ code_e2 ++ beq zero zero s2 ++ label s1 ++
+      lw a1 areg(4, a0) ++ lw a2 areg(8, a0) ++ sw a1 areg(adr1, fp) ++ sw a2 areg(adr2, fp) ++
+      code_e3 ++ label s2
+ 
+  | CDo l -> List.fold_left (fun c e -> compile_expr e ++ c) nop l
+
+  | CReturn -> nop
+
+  | CGlacon (f, l) -> let c, n1 = List.fold_left (fun (code,n) y ->
+                                                  code ++ lw a0 areg(y, fp) ++ sw a0 areg(n, v0)
+                                                  , n+4 )
+                                                 (nop, 8) l in
+                      li a0 8 ++ li v0 9 ++ syscall ++
+                      li a0 3 ++ sw a0 areg(0, v0) ++ move a2 v0 ++
+                      li a0 (4*(List.length l) + 8) ++ li v0 9 ++ syscall ++ sw v0 areg(4, a2) ++
+                      li a0 2 ++ sw a0 areg(0, v0) ++ la a0 alab f ++ sw a0 areg(4, v0) ++
+                      push a2 ++ c ++
+                      pop a0
 
 
-| _ -> nop
 (*
 | Cst i ->
         li a0 i ++ push a0
@@ -391,7 +494,8 @@ let rec compile_expr = function
       code ++ jal f ++ pop a0 ++ popn (4*List.length l) ++ push a0
 *)
 
-let compile_stmt (codefun, codemain) = function
+let compile_def (codefun, codemain) = function
+  
   | _ -> nop, nop
 (*
   | Set (x, e, fpmax) ->
@@ -427,7 +531,7 @@ let compile_program p ofile =
   let p = var_libre p in
   let p = ferm p in
   let p = alloc p in
-  let codefun, code = List.fold_left compile_stmt (nop, nop) p in
+  let codefun, code = List.fold_left compile_def (nop, nop) p in
   let p =
     { text =
         label "main" ++
