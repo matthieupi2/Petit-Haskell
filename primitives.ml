46ac26c598a290eb7edb73500b5a14c7c4cf849e
alban-pierre <alban.pierre@ens.fr>
2015-01-08 18:03:10 +0100

    Ajout de putChar et error

--- primitives.ml
+++ primitives.ml
@@ -9,15 +9,27 @@ type primitive = { name : string; typ : typ; generalized : bool;
     body : Mips.text; pdata : Mips.data } 
 
 (* TODO compléter les body *)
+(* ajouter ces fermetures dans le tas pour rester coherent avec *)
 let primitives = [
   { name = "div"; typ = Tarrow (Tint, Tarrow (Tint, Tint)); generalized = false;
-    body = nop ; pdata = nop} ;
+    body = nop
+(* _div : [a] x -> x/a
+   div : y -> _div [y] *)
+					; pdata = nop} ;
   { name = "rem"; typ = Tarrow (Tint, Tarrow (Tint, Tint)); generalized = false;
     body = nop ; pdata = nop } ;
-  { name = "putChar"; typ = Tarrow (Tchar, Tio); generalized = false;
-    body = nop ; pdata = nop } ;
+  { name = "putChar"; typ = Tarrow (Tchar, Tio); generalized = false; 
+    body = move v0 t0 ++ push ra ++ jal "_force" ++ pop ra ++ lw a0 areg(4, v0) ++
+           li v0 11 ++ syscall ++ jr ra
+					; pdata = nop } ;
   { name = "error"; typ = Tarrow (Tlist Tchar, Tvar (V.create ()));
-    generalized = true; body = nop; pdata = nop } ]
+    generalized = true;
+    body = move v0 t0 ++ push ra ++ jal "_force" ++ pop ra ++ label "_error_1" ++
+    lw a0 areg(0, v0) ++ beq a0 zero "_error_2" ++
+    lw a0 areg(8, v0) ++ push a0 ++ lw v0 areg(4, v0) ++ push ra ++ jal "_force" ++ pop ra ++
+    move t0 v0 ++ push ra ++ jal "putChar" ++ pop ra ++ pop v0 ++
+    beq zero zero "_error_1" ++ label "_error_2" ++ li a0 1 ++ li v0 17 ++ syscall
+					; pdata = nop } ]
 
 (* Utilisée par UncurriedAst pour vérifier la non redéfinition *)
 let getNames =
